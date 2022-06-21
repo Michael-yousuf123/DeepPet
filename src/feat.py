9b165edb2b2a02019df734bc233c100ee02e8862
miki <miki@miki>
2022-06-22 00:40:56 +0300

    Jupyter Update

--- src/feat.py
+++ src/feat.py
@@ -4,45 +4,56 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import RobustScaler
+TRAINPATH="/home/miki/Desktop/Deployment/bacillus-anthracis/data/traincsv/train.csv" 
+TESTPATH="/home/miki/Desktop/Deployment/bacillus-anthracis/data/testcsv/test.csv" 
 
 # create a function that import data and preprocess
 
-def data_load(path):
+def data_load(path = "."):
     """a function to import the data into
-    working environment 
+    working environment
+    ============================================== 
     ARGUMENTS: uses the path as an argument
-    RETURNS:
+    RETURNS:pandas dataframe
+    ==============================================
     """
-    if os.path.exists(path):
-        df = pd.read_csv(path,header= 0, index_col= 0,  parse_dates=True, squeeze = True)
-        df= df.replace('[./d.]', '', regex = True).astype(int)
+    if os.path.exists(path = "."):
+        df = pd.read_csv(path,header= 0, parse_dates=True, squeeze = True)
+        df['date'] = pd.to_datetime(df['date'])
+        df
+        return df
     else:
-        print("File doesn't exist")
-    return df.dropna()
-    
-def evenly_spaced(data):
-    df = data.pivot(index="date", columns="case", values="value")
-    final_df = df.ffill()
-    return final_df
-
-
-def data_normalize(df, x, y):
+        print("File doesn't exist")    
+def summary_statistics(x):
+    """"""
+    if x.dtypes == 'int64'or x.dtypes == 'float64':
+        return pd.DataFrame([[x.name, np.mean(x), np.std(x), np.median(x), np.var(x), np.min(x), np.max(x)]], 
+                            columns = ["Variable", "Mean", "Standard Deviation", "Median", "Variance", "Minimum", "Maximum"]).set_index("Variable")
+    else:
+        print("None")
+def check_index(x):
+    dups = ["date", "case"]
+    for dups in (x[x.index.duplicated()]):
+        print('The duplications are found')
+        x = x[~x.index.duplicated()]
+        return x
+    else:
+        print('No duplication is found')
+    return x
+def data_normalize(x):
     
     scaler = RobustScaler()
-    df = scaler.fit_transform(np.array(df[["x", "y"]]).reshape(-1, 1))
-    return df
-
-
-def data_split(data):
-    """funtion to split the dataset into train and 
-    test set using the argument
-    ==============================================
-    ARGUMENT: data = df"""
-    train, test = data[:670], data[670:]
-    test = test.reset_index(drop=True, inplace=True)
-    return train, test
-
-# def window_split(X= None, window_size = 12, end= None):
-#     """"""
-#     X = create_windows(X, window_shape = window_size, end_id= end)
-#     return X.shape
+    x = scaler.fit_transform(np.array(x).reshape(-1, 1)) 
+    return x  
+def create_trtst(data):
+    train_dfs=[]
+    val_dfs = []
+    train_size = int(len(data) * 0.8)
+    df_train = data.iloc[0:train_size], 
+    df_test = data.iloc[train_size:len(data)]
+    train_dfs.append(df_train)
+    val_dfs.append(df_test)
+    tr = pd.DataFrame([train_dfs])
+    te = pd.DataFrame([val_dfs])
+    tr.to_csv('TRAINPATH', index=False)
+    te.to_csv('TESTPATH', index=False)
